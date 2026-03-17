cc3832537d5a7b5d7392c30e639f2594cb7a8a3f
Oliver Drobnik <oliver@cocoanetics.com>
2026-03-17 13:55:29 +0100

    Update dependencies: SwiftText 1.1.4, SwiftMCP 1.4.0
    - SwiftText 1.1.4: Fixes blockquote paragraph breaks and leading blank lines
    - SwiftMCP 1.4.0: Stability improvements
    
    This should fix:
    - Paragraph breaks in quoted replies
    - Empty paragraphs at top of reply drafts

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Cocoanetics/SwiftMCP", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/Cocoanetics/SwiftMCP", .upToNextMajor(from: "1.4.0")),
         .package(url: "https://github.com/Cocoanetics/SwiftMail", .upToNextMajor(from: "1.3.2")),
-        .package(url: "https://github.com/Cocoanetics/SwiftText", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/Cocoanetics/SwiftText", .upToNextMajor(from: "1.1.4")),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
     ],
